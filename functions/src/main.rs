5778818c61571ad58424665bdad96125f32aa8a1
Ajit <ntn.ajit@gmail.com>
2020-10-28 07:48:40 +1100

    changed functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let x = plus_one(5);
 
-    another_function(5, 6);
+    println!("The value of x is: {}", x);
 }
 
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
