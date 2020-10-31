c9a239f7979e5bece0104daa585288c234d9df0e
Ajit <ntn.ajit@gmail.com>
2020-10-31 21:11:30 +1100

    done rectangles

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,20 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The are of the rectangle is {} square pixels", rect1.area());
 }
